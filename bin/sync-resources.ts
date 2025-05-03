1a3baf84392e5fb248c8352e84a1a5e2d310ae30
Cosmin Poieana <cosmin.poieana@a5labs.co>
2025-05-03 16:39:29 +0300

    Fix OpenAPI yml spec serving during building

--- bin/sync-resources.ts
+++ bin/sync-resources.ts
@@ -1,5 +1,9 @@
 import shell from "shelljs";
 
+// Ensure target directory exists
+shell.mkdir("-p", "dist/rest/v1");
+
 // Ensure Open API scheme.
 shell.cp("src/rest/v1/openapi.yml", "dist/rest/v1/");
+// We might still need the swagger initializer if using older swagger-ui-express or specific setups
 shell.cp("src/rest/v1/swagger-initializer.js", "dist/rest/v1/");
